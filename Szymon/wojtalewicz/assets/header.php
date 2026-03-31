a1fed1a9295c96e33e543221a69972b01134b994
Wojtalewicz0 <szymonwojtkiewicz01z@gamil.com>
2026-03-31 10:09:46 +0200

    update of update

---
+++ Szymon/wojtalewicz/assets/header.php
@@ -0,0 +1,10 @@
+    <header>
+		<h1>Hospital</h1>
+	</header>
+
+	<nav>
+		<a href="index.php">Home</a>
+		<a href="reg.php">Registration</a>
+		<a href="list.php">List of Patients</a>
+		<a href="get.php">Contact</a>
+	</nav>
