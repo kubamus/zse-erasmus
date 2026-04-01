636c6015b5545796b36039d25790462d3582ee47
danio-tech <97028933+danio-tech@users.noreply.github.com>
2026-04-01 12:58:57 +0200

    Added table

---
+++ Daniel/librabry-project/resources/views/templates/master.blade.php
@@ -0,0 +1,13 @@
+@include('templates.header')
+
+    @section('custom_styles')
+    @show
+
+    @section('content')
+    @show
+
+    @section('custom_scripts')
+    @show
+
+    
+@include('templates/footer')
